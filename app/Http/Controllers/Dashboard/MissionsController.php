7d939229f39fdd6639616aa197bcde4ebf89362a
Leonard Selvaraja <kashrayks@gmail.com>
2021-04-25 04:22:21 +0530

    Mission module (1/2) WIP

--- app/Http/Controllers/Dashboard/MissionsController.php
+++ app/Http/Controllers/Dashboard/MissionsController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Models\Mission;
+use App\Models\Twitter;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Exception;
 
 class MissionsController extends Controller
 {
-    //
+    public function index() {
+        $missions = Mission::orderBy('status')->get();
+        return view('dashboard.home.missions.index', [
+            'missions' => $missions,
+        ]);
+    }
+
+    public function view($uuid = '') {
+        if($uuid == '') {
+            notify()->error('Mission UUID was not passed', 'Whoops');
+            return redirect(route('home.mission.index'));
+        }
+
+        $mission = Mission::where('uuid', $uuid)->first();
+        if($mission) {
+            return view('dashboard.home.missions.view', [
+                'mission' => $mission,
+            ]);
+        } else {
+            notify()->error('This mission does not exist', 'Whoops');
+            return redirect(route('home.mission.index'));
+        }
+    }
 }
