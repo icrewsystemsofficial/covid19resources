03ca67d445b0dc3dee8cc09c0932eb6f2d7506ad
Leonard Selvaraja <kashrayks@gmail.com>
2021-04-25 22:23:28 +0530

    Worked on view  resoruce page, added URL sharer

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Dashboard;
 use App\Models\FAQ;
 use App\Models\User;
 use App\Models\States;
+use App\Models\Twitter;
+use App\Models\Referral;
 use App\Models\Resource;
 use App\Models\Districts;
+use App\Mail\WelcomeEmail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Referral;
-use App\Models\Twitter;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Auth\Events\Registered;
 use Spatie\Activitylog\Models\Activity as LogActivity;
 
 class HomeController extends Controller
@@ -35,22 +40,6 @@ class HomeController extends Controller
         $resources = Resource::
                         where('state', $this->currentlocation->name)
                         ->get();
-
-        // $tweets = Twitter::
-        //             where('status', Twitter::SCREENED)
-        //             ->orWhere('status', Twitter::VERIFIED)
-        //             ->orderBy('status', 'DESC')
-        //             ->get();
-
-        // $tweets_merge = array();
-
-        // foreach($tweets as $tweet) {
-
-        // }
-
-        // // dd($tweets[0]);
-        // dd($resources);
-
         return view('dashboard.home.home', [
             'faqs' => $faq,
             'states' => States::all(),
@@ -66,8 +55,6 @@ class HomeController extends Controller
 
         $user = User::where('referral_link', $referral)->first();
         if(!$user) {
-            //Incorrect referral.
-
             return redirect(route('home'));
         }
 
@@ -133,11 +120,48 @@ class HomeController extends Controller
 
     public function store_report(Request $request , $id) {
         $resource = Resource::find($id);
-        // dd($request->all());
+
+        $create_account = request('create_account');
+        if($create_account != null) {
+            //Find existing user by that email.
+            $existing_user = User::where('email', request('email'))->first();
+            if($existing_user) {
+                Auth::attempt(['email' => $existing_user->email, 'password' => request('password')]);
+                $user = $existing_user;
+            } else {
+                $request->validate([
+                    'name' => 'required|string|max:255',
+                    'email' => 'required|string|email|max:255|unique:users',
+                    'state' => 'required|string|max:30',
+                    'password' => 'required|string|confirmed|min:8',
+                ]);
+
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'state' => $request->state,
+                    'password' => Hash::make($request->password),
+                ]);
+
+
+                $user->assignRole('user');
+
+                event(new Registered($user));
+
+                Auth::login($user);
+
+                Mail::to($user->email)->send(new WelcomeEmail($user->name));
+            }
+        }
+
+
         if($request->reason == 1 || $request->reason == 2 || $request->reason == 3 || $request->reason == 4) {
-            $resource->verified = 2;
+            $resource->verified = Resource::REFUTED;
             $resource->save();
-            notify()->success('Your response were reported to admin');
+
+            // TODO: Add email here.
+
+            notify()->success('Your report was sent. Your effort goes a long way, we hope you find what you\'re looking for', 'Thank you '.$user->name);
             return redirect(route('home'));
         } else {
             notify()->error('Some error occured try again', 'Whoops');
