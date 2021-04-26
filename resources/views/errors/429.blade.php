8f8331536115da9be8a6b0d21864559f92db2146
Leonard Selvaraja <kashrayks@gmail.com>
2021-04-27 05:16:03 +0530

    error pages changed

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Too Many Requests'))
 @section('code', '429')
-@section('message', __('Too Many Requests'))
+@section('message', 'The application is facing unusual amount of traffic, please wait and refresh in a minute or so. We appreciate your patience.')
