f60f7888ea63c62de8d33c62293998d0ad33e404
Leonard Selvaraja <kashrayks@gmail.com>
2021-05-14 00:16:15 +0530

    API Index page added

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
+
+    Route::get('/', function() {
+        $response = array([
+            'message' => config('app.name').' API Version 1 Operational',
+        ]);
+        return response($response, 200);
+    });
+
     Route::get('/states', [States::class, 'getStates']);
     Route::get('/districts/{code?}', [States::class, 'getDistricts']);
     Route::get('/cities/{state?}', [States::class, 'getCities']);
